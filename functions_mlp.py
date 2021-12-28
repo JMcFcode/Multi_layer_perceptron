Successfully got network up to about 30% accuracy: 1) Fixed bug with One Hot encoding. 2) Fixed bug with the weights being calcuated in the correct way. 3) Fixed major bug that calculates the average of a numpy array while preserving its shape.
Added more Comments into the code.

functions_mlp.py
@@ -7,6 +7,7 @@ Created on Sun Dec 19 08:46:58 2021
 
 Building a multi_layer_perceptron from the ground up
 """
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -24,14 +25,16 @@ class Network:
     """
 
     def __init__(self):
-        self.alpha = 1
-        self.dict_network = {}
+        self.learning_rate = 5
+        # self.list_nodes = [16, 10]
+
         self.read_data()
         self.create_network()
-        self.iter_size = 50
-        self.learning_rate = 0.2
 
     def read_data(self):
+        """
+        Read in the data from the csv files that should be saved in the same directory as this file.
+        """
         self.test_df = pd.read_csv('mnist_test.csv')
         self.train_df = pd.read_csv('mnist_train.csv')
 
@@ -39,15 +42,29 @@ class Network:
         self.train_matrix, self.train_labels = self.create_matrix(df=self.train_df)
 
     def create_network(self):
-        self.w = {'w1': np.random.rand(16, 784) - 0.5,
-                  'w2': np.random.rand(10, 16) - 0.5}
+        """
+        Manually write in the size of the weights and biases for each layer of the network.
+        :return:
+        """
+        self.w = {'w1': np.random.randn(16, 784),
+                  'w2': np.random.randn(10, 16)}
 
-        self.b = {'b1': np.random.rand(16) - 0.5,
-                  'b2': np.random.rand(10) - 0.5}
+        self.b = {'b1': np.random.randn(16),
+                  'b2': np.random.randn(10)}
 
-    def feedforward(self, image_data: np.ndarray):
+    # def create_network(self):
+    #     """
+    #     Automatically create the weights and biases from self.list_nodes.
+    #     """
+    #     self.w = {}
+    #     self.b = {}
+    #     for i in range(len(self.list_nodes)):
+    #         self.w['w'+str(i)] = np.random.randn(self.list_nodes[i], 784)
+    #         self.b['b'+str(i)] = np.random.randn(self.list_nodes[i])
+
+    def feedforward(self, image_data: np.ndarray) -> dict:
         res = {}
-        res['image'] = image_data
+        res['a0'] = image_data
         res['z1'] = np.dot(self.w['w1'], image_data) + self.b['b1']
         res['a1'] = self.sigmoid(res['z1'])
 
@@ -61,7 +78,7 @@ class Network:
         """
         This is for turning the original dataset into useful values.
         :param df:
-        :return: two arrays
+        :return: two arrays, one with the matrix and one with the label vector!
         """
         matrix = np.array(df.drop(columns=['label'])).T
         labels = np.array(df['label'])
@@ -71,35 +88,41 @@ class Network:
     def sigmoid(x_array):
         return 1 / (1 + np.exp(-x_array))
 
-    @staticmethod
-    def sig_deriv(x_array: np.ndarray) -> np.ndarray:
-        out = np.exp(x_array) / np.power((1 + np.exp(x_array)), 2)
-        return np.nan_to_num(out)
+    def sig_deriv(self, x_array: np.ndarray) -> np.ndarray:
+        out = self.sigmoid(x_array) * (1 - self.sigmoid(x_array))
+        return out
 
     @staticmethod
     def one_hot(label_vec: np.ndarray) -> np.ndarray:
+        """
+        One hot encode the labels to make use of them easier later.
+        :param label_vec:
+        :return: digit_mat
+        """
         digit_mat = np.zeros((10, label_vec.size))
         for i in range(len(label_vec)):
-            digit_mat[label_vec[i] - 1, i] = 1
+            digit_mat[label_vec[i], i] = 1
         digit_mat = digit_mat
         return digit_mat
 
     def back_prop(self, array_data: np.ndarray, array_labels: np.ndarray) -> dict:
         """
         Calculate the cost vector.
+        Backpropogate the errors through the network to adjust both the weights and the biases.
         """
         dict_vals = {'dw2': [], 'db2': [], 'dw1': [], 'db1': [], 'Cost': []}
         for i in range(array_data.shape[1]):
             labels = array_labels[:, i]
             res = self.feedforward(image_data=array_data[:, i])
 
-            db2_array = self.sig_deriv(res['z2']) * 2 * (res['a2'] - labels)
-            dw2_array = np.outer(db2_array, res['a1'])
+            delta = self.sig_deriv(res['z2']) * 2 * (res['a2'] - labels)
+            db2_array = delta
+            dw2_array = np.outer(delta, res['a1'].T)
 
-            delta_1 = np.dot(self.w['w2'].T, 2 * (res['a2'] - labels) * self.sig_deriv(res['z2']))
+            delta = np.dot(self.w['w2'].T, delta) * self.sig_deriv(res['z1'])
 
-            db1_array = self.sig_deriv(res['z1']) * delta_1
-            dw1_array = np.outer(db1_array, res['image'])
+            db1_array = delta
+            dw1_array = np.outer(delta, res['a0'].T)
 
             dict_vals['dw2'].append(dw2_array)
             dict_vals['db2'].append(db2_array)
@@ -107,19 +130,29 @@ class Network:
             dict_vals['dw1'].append(dw1_array)
             dict_vals['db1'].append(db1_array)
 
-            dict_vals['Cost'].append(np.sum(np.power((res['a2'] - labels), 2)))
+            dict_vals['Cost'].append(np.sum((res['a2'] - labels) ** 2))
 
-        end_vals = {}
-        end_vals['dw2'] = 1 / len(array_data) * np.sum(dict_vals['dw2'])
-        end_vals['db2'] = 1 / len(array_data) * np.sum(dict_vals['db2'])
+        end_vals = {'dw2': self.av_array(dict_vals['dw2']),
+                    'db2': self.av_array(dict_vals['db2']),
+                    'dw1': self.av_array(dict_vals['dw1']),
+                    'db1': self.av_array(dict_vals['db1']),
+                    'Average_Cost': np.mean(dict_vals["Cost"])}
 
-        end_vals['dw1'] = 1 / len(array_data) * np.sum(dict_vals['dw1'])
-        end_vals['db1'] = 1 / len(array_data) * np.sum(dict_vals['db1'])
-
-        end_vals['Average_Cost'] = np.mean(dict_vals["Cost"])
         print(f'Average Cost: {end_vals["Average_Cost"]}')
 
         return end_vals
+
+    @staticmethod
+    def av_array(list_array: list) -> np.ndarray:
+        """
+        Calculate the average of a list of numpy vectors,
+        :param list_array:
+        :return: np.ndarray:
+        """
+        out = np.zeros(shape=list_array[0].shape)
+        for arr in list_array:
+            out += arr
+        return 1 / len(list_array) * out
 
     def sgd(self, num_per_iter: int, num_iter: int, draw_cost=False):
         """
@@ -141,7 +174,7 @@ class Network:
             self.b['b2'] = self.b['b2'] - vals_dict['db2'] * self.learning_rate
 
             cost_list.append(vals_dict['Average_Cost'])
-            print(f'Generation {i} complete.')
+            # print(f'Generation {i} complete.')
 
         if draw_cost:
             plt.plot(range(num_iter), cost_list)
@@ -149,9 +182,39 @@ class Network:
             plt.ylabel('Cost')
             plt.title('Average Cost as a function of Generation Trained')
 
-    def test_model(self):
-        for image in self.test_matrix:
-            pass
+    def test_model(self) -> pd.DataFrame:
+        """
+        Test out the models accuracy on the test data provided.
+        :return: df
+        """
+        list_corr = []
+        list_preds = []
+        for i in range(self.test_matrix.shape[1]):
+            image = self.test_matrix[:, i]
+            actual_val = self.test_labels[i]
+            res = self.feedforward(image_data=image)
+            pred_val = np.argmax(res['a2'])
+            if pred_val == actual_val:
+                list_corr.append(1)
+            else:
+                list_corr.append(0)
+            list_preds.append([pred_val, actual_val])
+        df = pd.DataFrame(list_preds, columns=['Predictions', 'Actual'])
+        print(f'Percentage Correct is: {100 * sum(list_corr) / len(list_corr)}%')
+        return df
 
-    def draw_number(self):
-        pass
+    def draw_number(self, val: int):
+        """
+        Method that will draw the number trying to be read.
+        Method will also tell you our prediction and the actual value
+        :param val:
+        """
+        image = self.test_matrix[:, val]
+        actual_val = self.test_labels[val]
+        res = self.feedforward(image_data=image)
+        pred_val = np.argmax(res['a2'])
+        print(f'Actual Value: {actual_val}')
+        print(f'Predicted Value: {pred_val}')
+        image = self.test_matrix[:, val].reshape(28, 28)
+        plt.imshow(image, cmap='gray')
+        plt.show()